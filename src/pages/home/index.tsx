e139697c719032b3de921ab7b0a90e0a3264ef49
Heyff12 <645068564@qq.com>
2019-03-31 11:32:45 +0800

    second

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,7 @@ class Home extends React.Component<{}, {}> {
   render() {
     return (
       <>
-        <h1>Hello,world!第一次commit提交forCI</h1>
+        <h1>Hello,world!第一次commit提交forCI---second--push--trigger</h1>
       </>
     );
   }
